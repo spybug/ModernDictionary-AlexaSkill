2fb67f4f0424e528e78bad03dfda9521233be919
Karl <spybug@users.noreply.github.com>
2017-07-29 20:37:31 -0700

    Added way to get a different word if it contains any profanities

--- modernDictionaryAlexaSkill.js
+++ modernDictionaryAlexaSkill.js
@@ -1,77 +1,38 @@
 var https = require('https');
+var swearjar = require('swearjar');
 var appID = process.env['app_ID'];
 
-console.log(appID);
-
 exports.handler = (event, context) => {
-
     try {
-        if (event.session.new) {
-            console.log('NEW SESSION');
-        }
         if (event.session.application.applicationId != appID) {
             throw "Not a valid App ID";
         }
 
         switch (event.request.type) {
             case 'LaunchRequest':
-                console.log('LAUNCH REQUEST');
-                context.succeed(
-                    generateResponse(
-                        buildSpeechletResponse('Modern Dictionary is a skill to learn new funny phrases and words by giving you the definition. Please ask for a random definition now or say stop to exit.', false),
-                        {}
-                    )
-                );
+                launchText = 'Modern Dictionary is a skill to learn new funny phrases and words by giving you the definition. Please ask for a random definition now or say stop to exit.';
+                returnResponse(launchText, false);
                 break;
 
             case 'IntentRequest':
-                console.log('INTENT REQUEST');
-                var endpoint = '';
-                var body = '';
                 switch(event.request.intent.name) {
                     case 'GetRandomDefinition':
-                        endpoint = 'https://api.urbandictionary.com/v0/random';
-                        https.get(endpoint, (response) => {
-                            response.on('data', (chunk) => { body += chunk; });
-                            response.on('end', () => {
-                                var data = JSON.parse(body);
-                                var definition = data.list[0].definition;
-                                var word = data.list[0].word;
-                                var result = 'The random word is ' + word + '. The definition is ' + definition + '.';
-                                console.log('word: ' + word);
-                                context.succeed(
-                                    generateResponse(
-                                        buildSpeechletResponse(result, true),
-                                        {}
-                                    )
-                                );
-                            });
-                        });
+                        getRandomDefinition();
                         break;
 
                     case 'AMAZON.CancelIntent':
                     case 'AMAZON.StopIntent':
-                        context.succeed(
-                            generateResponse(
-                                buildSpeechletResponse("Goodbye", true),
-                                {}
-                            )
-                        );
+                        returnResponse("Goodbye", true);
                         break;
 
                     case 'AMAZON.HelpIntent':
-                        context.succeed(
-                            generateResponse(
-                                buildSpeechletResponse("Modern Dictionary gives modern day definitions to words and phrases. To hear one, please ask Modern Dictionary for a random word now.", false),
-                                {}
-                            )
-                        );
+                        helpText = "Modern Dictionary gives modern day definitions to words and phrases. To hear one, please ask Modern Dictionary for a random word now.";
+                        returnResponse(helpText, false);
                         break;
                 }
                 break;
 
             case 'SessionEndedRequest':
-                console.log('Session Ended Request');
                 break;
 
             default:
@@ -80,11 +41,51 @@ exports.handler = (event, context) => {
     }
     catch(error) { context.fail('Exception: $(error)'); }
 
+    function getRandomDefinition() {
+        var endpoint = 'https://api.urbandictionary.com/v0/random';
+        var body = '';
+        https.get(endpoint, (response) => {
+            response.on('data', (chunk) => { body += chunk; });
+            response.on('end', () => {
+                var data = JSON.parse(body);
+                var foundCleanWord = false
+                for (var i = 0; i < data.list.length; i++) {
+                    var word = data.list[i].word;
+                    // Check to see if the word contains profanities
+                    if (!swearjar.profane(word)) {
+                        var definition = data.list[i].definition;
+                        // Censor the definition of profanities
+                        cleanDefinition = swearjar.censor(definition);
+
+                        var result = 'The random word is ' + word + '. The definition is ' + cleanDefinition;
+                        foundCleanWord = true;
+                        returnResponse(result, true);
+                    }
+                    else {
+                        // Useful for debugging retries
+                        console.log('Profane word was ' + word + ", retrying...");
+                        continue;
+                    }
+                }
+                if (!foundCleanWord) {
+                    returnResponse("Sorry I was not able to find a clean defintion, please try again later", true);
+                }
+            });
+        });
+    }
+
+    function returnResponse(text, shouldEndSession) {
+        context.succeed(
+            generateResponse(
+                buildSpeechletResponse(text, shouldEndSession),
+                {}
+            )
+        );
+    }
 };
 
 // Helpers
 buildSpeechletResponse = (outputText, shouldEndSession) => {
-
   return {
     outputSpeech: {
       type: 'PlainText',
@@ -92,15 +93,12 @@ buildSpeechletResponse = (outputText, shouldEndSession) => {
     },
     shouldEndSession: shouldEndSession
   };
-
 };
 
 generateResponse = (speechletResponse, sessionAttributes) => {
-
   return {
     version: '1.0',
     sessionAttributes: sessionAttributes,
     response: speechletResponse
   };
-
 };
